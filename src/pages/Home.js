ccdb42137602d10ab8512332225e917e382e16a2
nathan_rand <nrand02@vt.edu>
2022-07-26 20:21:58 -0500

    Got the header component working besides setting scroll positions, which I will do at a later date once ScrollMagic is integrated (assuming I still use that package)... :D

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,50 @@
+import styled from "styled-components";
+
+import { Header } from "../components/Header"
+
+import starryBackground from '../img/Starry-Background-Simple.png'
+
 export const Home = () => {
+  //Styling for the Home page component
+  const HomeContainer = styled.div`
+    width:100%;
+    height:100vh;
+    display:flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    background-image: url(${starryBackground});
+    background-size: 100% auto;
+    background-repeat:repeat-y;
+  `;
+
+  //Defines the header items used within the Home Page's Header
+  const headerItems = [
+    {
+      key:0,
+      value:'Home',
+      scrollPosition:''
+    },
+    {
+      key:1,
+      value:'About Me',
+      scrollPosition:''
+    },
+    {
+      key:2,
+      value:'Projects',
+      scrollPosition:''
+    },
+    {
+      key:3,
+      value:'Contact',
+      scrollPosition:''
+    }
+  ];
+
   return (
-    <div>Home</div>
+    <HomeContainer>
+      <Header navItems={headerItems}></Header>
+    </HomeContainer>
   )
 }
