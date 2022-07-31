b1f7bc40dd969b770ea477af1c95212bd030bd7f
nathan_rand <nrand02@vt.edu>
2022-07-31 12:18:34 -0500

    Got the animation for turning and thrusting completed. Next I want to clean things up regarding the turning portion and then I will most likely go on to designing my project planets and working on some design before more home page stuff :D

--- src/components/Rocket/Rocket.js
+++ src/components/Rocket/Rocket.js
@@ -1,20 +1,72 @@
 import { RocketIcon } from "./styleRocket"
-import rocketImage from "../../img/Duck-In-Rocket.png"
+import rocketImageStationary from "../../img/Duck-In-Rocket.png"
+import rocketImageThrusting from "../../img/Duck-In-Rocket-With-Thrust.png"
 import { gsap, Linear} from "gsap"
 import { useEffect, useRef } from "react"
 
 export const Rocket = () => {
-  //Animation for the rocket effects
-  let rocketRef = useRef(null);
-  useEffect(() => {
+  //Sets up support variables for switching between images for the engine's being on and off, as well as it's current direction (1 is down, -1 is up)
+  let rocketImage = rocketImageStationary;
+  let rocketDirection = 1;
+  //Support function that "toggles the engines" for the rocket by switching the image paths
+  let toggleEngines = (enginesOff) => {
+    if(enginesOff){
+      console.log("Toggling Engines Off");
+      gsap.set(rocketRef.current, { attr: { src: rocketImageStationary } });
+    }
+    else{
+      console.log("Toggling Engines On");
+      gsap.set(rocketRef.current, { attr: { src: rocketImageThrusting } });
+    }
+  }
+  //Support function for turning the rocket around
+  let turn = (direction) => {
+    //Set the rotation value based on scroll direction
+    let rotation = 0;
+    if(direction === 1){
+      rotation = 180;
+    }
+    //Animate the rotation
     gsap.to(rocketRef.current, {
-      scrollTrigger: {
-        scrub: 1,
-      },
-      y: '160vh',
+      rotation:rotation,
       ease:Linear.easeNone,
+      duration:0.25,
+      onComplete: console.log("Rotating Complete"),
+    });
+    //Update directional field
+    rocketDirection = direction;
+  }
+  //Timeline for the rocket animations
+  let rocketRef = useRef(null);
+ useEffect(() => {
+    let timeline = gsap.timeline({
+      scrollTrigger:{
+        scrub:1,
+        onLeaveBack: () => {toggleEngines(true)},
+        onUpdate: (self) => {
+          if(rocketDirection !== self.direction){
+            turn(self.direction);
+          }
+        }
+      },
+    });
+    //Animation for rotating at the start of the page to face the correct direction
+    timeline.to(rocketRef.current, {
+      rotation:-180,
+      y:'5vh',
+      ease:Linear.easeNone,
+      repeat:0,
+      duration:0.05,
+      onComplete: toggleEngines,
+      onCompleteParams: [false],
     })
-  })
+    //Animation for flying up and down throughout the page
+    timeline.to(rocketRef.current, {
+      y: '157.5vh',
+      ease:Linear.easeNone,
+      duration:0.95,
+    });
+ });
   return (
     <RocketIcon src={rocketImage} ref={rocketRef}></RocketIcon>
   )
