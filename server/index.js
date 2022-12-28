52ff65f70e2986bd6858e9fb7d282e74a9216631
nathan_rand <nrand02@vt.edu>
2022-12-28 15:32:49 -0500

    Added express support for parsing input values and putting them into the SendGrid email and sending to my personal email. Now all that is needed is the connection with the front-end and we balling

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ Version: 1.0
 
 const express = require('express');
 const app = express();
+const sgMail = require('@sendgrid/mail');
 const port = 5000;
 
 // Use express JSON middleware
@@ -17,14 +18,57 @@ app.use(express.json());
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+// Gets the API Key for Sendgrid from the environment variables
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 app.post('/contact', (req, res) => {
   let { package } = req.body;
-  
+  //ensure that all pertinent information was included in the request
   if(!package){
-    res.status(418).send({message: 'We need a contact email package!'});
+    res.status(418).send({message: 'We need a contact email package to send the email!'});
+  }
+  else if(!package.firstName){
+    res.status(418).send({message: 'A first name is required'});
+  }
+  else if(!package.lastName){
+    res.status(418).send({message: 'A last name is required'});
+  }
+  else if(!package.email){
+    res.status(418).send({message: 'An email is required'});
+  }
+  else if(!package.phoneNumber){
+    res.status(418).send({message: 'A phone number is required'});
+  }
+  else if(!package.message){
+    res.status(418).send({message: 'A message is required'});
   }
 
-  res.status(200).send({
-    package: `Our message package contains: ${package.firstName}`
-  })
+  // If everything is included, unpack the email package from the front-end
+  let firstName = package.firstName;
+  let lastName = package.lastName;
+  let email = package.email;
+  let phoneNum = package.phoneNumber;
+  let message = package.message;
+
+  // Define our email msg
+  const msg = {
+    to: 'nrand02@vt.edu', // Change to your recipient
+    from: 'contactMe@em3856.nathanrand.dev', // Change to your verified sender
+    subject: 'Portfolio Website Contact Message',
+    text: `Name: ${firstName} ${lastName}\n\nEmail: ${email}\nPhone Number: ${phoneNum}\n\nMessage: ${message}`
+  }
+  
+  // Send the email package to my personal email
+  sgMail
+    .send(msg)
+    .then((response) => {
+      console.log(response[0].statusCode)
+      console.log(response[0].headers)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  
+  // Resolve the response
+  res.end();
 })
